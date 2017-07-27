b05bf55ed48f7e7d010540dbc187a360f286e6df
Hari P <harip@Haris-MacBook-Pro-2.local>
2017-07-27 23:02:44 +0530

    Enums

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -41,5 +41,44 @@ if let convertedRank = Rank(rawValue:3) {
 
 
 
+enum Suit{
+    case spades, hearts, diamonds, clubs
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
