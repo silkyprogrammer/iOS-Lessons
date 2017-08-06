cfa60d740267d817d363dadae790f4f5f643d072
Hari P <harip@Haris-MacBook-Pro-2.local>
2017-08-06 16:50:20 +0530

    Apps in swift

--- Protocol.playground/Contents.swift
+++ Protocol.playground/Contents.swift
@@ -13,4 +13,26 @@ protocol ExampleProtocol{
 
 class SimpleClass:ExampleProtocol{
     var simpleDescription: String = "A Simple Class"
+    var anotherProperty:Int = 69105
+    
+    func adjust() {
+        simpleDescription += "Now 100% adjusted"
+    }
 }
+
+var a = SimpleClass()
+a.adjust()
+
+let aDesc = a.simpleDescription
+
+struct SimpleStructure : ExampleProtocol{
+    var simpleDescription: String = "A Simple structure"
+    mutating func  adjust() {
+        simpleDescription += "(adjusted)"
+    }
+    
+}
+
+var b = SimpleStructure()
+b.adjust()
+let bDesc = b.simpleDescription
